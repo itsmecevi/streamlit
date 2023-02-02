e4ddf0981728040814dc2826a5750d70ff328222
Cevi Herdian Insight <itsmecevi@gmail.com>
2023-02-02 11:14:55 +0700

    Add files via upload

---
+++ API Reference/st_chart_element.py
@@ -0,0 +1,410 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("Chart Element")
+
+
+st.subheader("st.line_chart:")
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+st.subheader("st.area_chart:")
+chart_data = pd.DataFrame(
+    np.random.randn(20, 4),
+    columns=['a', 'b', 'c', "d"])
+
+st.area_chart(chart_data)
+
+
+st.subheader("st.bar_chart:")
+chart_data = pd.DataFrame(
+    np.random.randn(5, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
+
+st.subheader("st.pyplot:")
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=5)
+
+st.pyplot(fig)
+
+
+st.subheader("st.altair_chart:")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+chart_data = pd.DataFrame(
+    np.random.randn(10, 3),
+    columns=['a', 'b', 'c'])
+
+c = alt.Chart(chart_data).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+
+st.altair_chart(c, use_container_width=True)
+
+import altair as alt
+from vega_datasets import data
+
+source = data.cars()
+
+chart = alt.Chart(source).mark_circle().encode(
+    x='Horsepower',
+    y='Miles_per_Gallon',
+    color='Origin',
+).interactive()
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
+
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Altair theme.
+    st.altair_chart(chart, theme=None, use_container_width=True)
+
+
+
+    import altair as alt
+import streamlit as st
+from vega_datasets import data
+
+source = data.seattle_weather()
+
+scale = alt.Scale(
+    domain=["sun", "fog", "drizzle", "rain", "snow"],
+    range=["#e7ba52", "#a7a7a7", "#aec7e8", "#1f77b4", "#9467bd"],
+)
+color = alt.Color("weather:N", scale=scale)
+
+# We create two selections:
+# - a brush that is active on the top panel
+# - a multi-click that is active on the bottom panel
+brush = alt.selection_interval(encodings=["x"])
+click = alt.selection_multi(encodings=["color"])
+
+# Top panel is scatter plot of temperature vs time
+points = (
+    alt.Chart()
+    .mark_point()
+    .encode(
+        alt.X("monthdate(date):T", title="Date"),
+        alt.Y(
+            "temp_max:Q",
+            title="Maximum Daily Temperature (C)",
+            scale=alt.Scale(domain=[-5, 40]),
+        ),
+        color=alt.condition(brush, color, alt.value("lightgray")),
+        size=alt.Size("precipitation:Q", scale=alt.Scale(range=[5, 200])),
+    )
+    .properties(width=550, height=300)
+    .add_selection(brush)
+    .transform_filter(click)
+)
+
+# Bottom panel is a bar chart of weather type
+bars = (
+    alt.Chart()
+    .mark_bar()
+    .encode(
+        x="count()",
+        y="weather:N",
+        color=alt.condition(click, color, alt.value("lightgray")),
+    )
+    .transform_filter(brush)
+    .properties(
+        width=550,
+    )
+    .add_selection(click)
+)
+
+chart = alt.vconcat(points, bars, data=source, title="Seattle Weather: 2012-2015")
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Altair native theme"])
+
+with tab1:
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
+with tab2:
+    st.altair_chart(chart, theme=None, use_container_width=True)
+
+
+
+import altair as alt
+import pandas as pd
+import streamlit as st
+from vega_datasets import data
+
+# We use @st.experimental_memo to keep the dataset in cache
+@st.experimental_memo
+def get_data():
+    source = data.stocks()
+    source = source[source.date.gt("2004-01-01")]
+    return source
+
+source = get_data()
+
+# Define the base time-series chart.
+def get_chart(data):
+    hover = alt.selection_single(
+        fields=["date"],
+        nearest=True,
+        on="mouseover",
+        empty="none",
+    )
+
+    lines = (
+        alt.Chart(data, title="Evolution of stock prices")
+        .mark_line()
+        .encode(
+            x="date",
+            y="price",
+            color="symbol",
+        )
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = lines.transform_filter(hover).mark_circle(size=65)
+
+    # Draw a rule at the location of the selection
+    tooltips = (
+        alt.Chart(data)
+        .mark_rule()
+        .encode(
+            x="yearmonthdate(date)",
+            y="price",
+            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
+            tooltip=[
+                alt.Tooltip("date", title="Date"),
+                alt.Tooltip("price", title="Price (USD)"),
+            ],
+        )
+        .add_selection(hover)
+    )
+    return (lines + points + tooltips).interactive()
+
+chart = get_chart(source)
+
+# Add annotations
+ANNOTATIONS = [
+    ("Mar 01, 2008", "Pretty good day for GOOG"),
+    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
+    ("Nov 01, 2008", "Market starts again thanks to..."),
+    ("Dec 01, 2009", "Small crash for GOOG after..."),
+]
+annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
+annotations_df.date = pd.to_datetime(annotations_df.date)
+annotations_df["y"] = 10
+
+annotation_layer = (
+    alt.Chart(annotations_df)
+    .mark_text(size=20, text="👇", dx=-8, dy=-10, align="left")
+    .encode(
+        x="date:T",
+        y=alt.Y("y:Q"),
+        tooltip=["event"],
+    )
+    .interactive()
+)
+
+st.altair_chart(
+    (chart + annotation_layer).interactive(),
+    use_container_width=True
+)
+
+
+st.subheader("st.vega_lite_chart:")
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+
+st.vega_lite_chart(chart_data, {
+    'mark': {'type': 'circle', 'tooltip': True},
+    'encoding': {
+        'x': {'field': 'a', 'type': 'quantitative'},
+        'y': {'field': 'b', 'type': 'quantitative'},
+        'size': {'field': 'c', 'type': 'quantitative'},
+        'color': {'field': 'c', 'type': 'quantitative'},
+    },
+})
+
+
+
+import streamlit as st
+from vega_datasets import data
+
+source = data.cars()
+
+chart = {
+    "mark": "point",
+    "encoding": {
+        "x": {
+            "field": "Horsepower",
+            "type": "quantitative",
+        },
+        "y": {
+            "field": "Miles_per_Gallon",
+            "type": "quantitative",
+        },
+        "color": {"field": "Origin", "type": "nominal"},
+        "shape": {"field": "Origin", "type": "nominal"},
+    },
+}
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Vega-Lite native theme"])
+
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.vega_lite_chart(
+        source, chart, theme="streamlit", use_container_width=True
+    )
+with tab2:
+    st.vega_lite_chart(
+        source, chart, theme=None, use_container_width=True
+    )
+
+
+st.subheader("st.plotly_chart:-X")
+
+
+import plotly.express as px
+import streamlit as st
+
+df = px.data.gapminder()
+
+fig = px.scatter(
+    df.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
+
+
+st.subheader("st.bokeh_chart:-X")
+
+
+st.subheader("st.pydeck_chart:")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
+
+
+st.subheader("st.graphviz_chart:")
+import streamlit as st
+import graphviz
+
+# Create a graphlib graph object
+graph = graphviz.Digraph()
+graph.edge('run', 'intr')
+graph.edge('intr', 'runbl')
+graph.edge('runbl', 'run')
+graph.edge('run', 'kernel')
+graph.edge('kernel', 'zombie')
+graph.edge('kernel', 'sleep')
+graph.edge('kernel', 'runmem')
+graph.edge('sleep', 'swap')
+graph.edge('swap', 'runswap')
+graph.edge('runswap', 'new')
+graph.edge('runswap', 'runmem')
+graph.edge('new', 'runmem')
+graph.edge('sleep', 'runmem')
+
+st.graphviz_chart(graph)
+
+st.graphviz_chart('''
+    digraph {
+        run -> intr
+        intr -> runbl
+        runbl -> run
+        run -> kernel
+        kernel -> zombie
+        kernel -> sleep
+        kernel -> runmem
+        sleep -> swap
+        swap -> runswap
+        runswap -> new
+        runswap -> runmem
+        new -> runmem
+        sleep -> runmem
+    }
+''')
+
+
+st.subheader("st.map:")
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
